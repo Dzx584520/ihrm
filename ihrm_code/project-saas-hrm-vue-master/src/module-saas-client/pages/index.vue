141dfc877d5af01384f756362abd010bae931096
dingzixuan <584321579@qq.com>
2022-08-10 22:22:42 +0800

    前端修改

---
+++ ihrm_code/project-saas-hrm-vue-master/src/module-saas-client/pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="dashboard-container">
+    saas企业管理
+  </div>
+</template>
+
+<script>
+import {list} from '@/api/example/table'
+export default {
+  name: 'saas-clients-table-index',
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  },
+  // 创建完毕状态
+  created() {
+  },
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.alert {
+  margin: 10px 0px 0px 0px;
+}
+.pagination {
+  margin-top: 10px;
+  text-align: right;
+}
+</style>
